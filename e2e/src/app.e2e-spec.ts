b3295f99e5688997cd29ed8e48050b9e34f6d672
nerdfiles <nerdfiles@gmail.com>
2020-04-22 14:53:36 -0500

    fix teh e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { AppPage } from './app.po';
 
-describe('new App', () => {
+describe('AirVisual Frontend', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display a sensible page', () => {
     page.navigateTo();
-    expect(page.getPageTitle()).toContain('Tab 1');
+    expect(page.getPageTitle()).toContain('Configure');
   });
 });
