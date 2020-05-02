46d61a519a14901ad2b087daf61e016077acdab5
nerdfiles <nerdfiles@gmail.com>
2020-05-01 19:33:41 -0500

    etc

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -3,6 +3,12 @@ import {
   DATA_REQUESTED
 } from '../constants/action-types';
 
+/**
+ * getData
+ *
+ * @param url
+ * @returns {object}
+ */
 export function getData(url) {
   return {
     type: DATA_REQUESTED,
--- src/app/sagas/api-saga.ts
+++ src/app/sagas/api-saga.ts
@@ -1,22 +1,46 @@
 import { takeEvery, call, put } from 'redux-saga/effects';
 
-import { DATA_LOADED, DATA_REQUESTED, API_ERRORED } from '../constants/action-types';
+import {
+  DATA_LOADED,
+  DATA_REQUESTED,
+  API_ERRORED
+} from '../constants/action-types';
 
 import { environment } from '../env';
 
+/**
+ * watcherSaga
+ *
+ */
 export default function* watcherSaga() {
   yield takeEvery(DATA_REQUESTED, workerSaga);
 }
 
+/**
+ * workerSaga
+ *
+ * @param action An object containing a payload.
+ */
 function* workerSaga(action) {
   try {
     const payload = yield call(getData, action.payload.url);
-    yield put({ type: DATA_LOADED, payload });
+    yield put({
+      type: DATA_LOADED,
+      payload
+    });
   } catch(e) {
-    yield put({ type: API_ERRORED, payload: e });
+    yield put({
+      type: API_ERRORED,
+      payload: e
+    });
   }
 }
 
+/**
+ * getData
+ *
+ * @param url A URL.
+ */
 function getData(url):Promise<any[]> {
   return fetch(url, {
     method: 'GET',
